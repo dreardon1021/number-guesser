73922e22e6549c968518ce121cc6a6dc3c93c239
Dan Reardon <dreardon1021@gmail.com>
2019-12-05 16:43:18 -0700

    mentor console logs added for assitance

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 
 for(var i = 0; i < document.querySelectorAll('.middle-left-input').length; i++){
-document.querySelectorAll('.middle-left-input')[i].addEventListener('keyup', enableSubmitButton)
+document.querySelectorAll('.middle-left-input')[i].addEventListener('keypress', enableSubmitButton)
 };
 
+
+// function enableSubmitButton(){
+//   var submitButton = document.querySelector('.submit-button');
+//   var inputFields = document.querySelectorAll('.middle-left-input');
+//     for(var i = 0; i < inputFields.length; i++){
+//     console.log(inputFields[i])
+//     if (inputFields[i].value < 5){
+//        submitButton.disabled = true;
+//      } else {
+//       submitButton.disabled = false;
+//     };
+//   };
+// };
 
 function enableSubmitButton(){
   var submitButton = document.querySelector('.submit-button');
   var inputFields = document.querySelectorAll('.middle-left-input');
     for(var i = 0; i < inputFields.length; i++){
-    if (inputFields[i].value > 0){
+    console.log('inputFields:', inputFields);
+    console.log('inputFields[i]:', inputFields[i]);
+    console.log('inputFields[i].value:', inputFields[i].value);
+    if (inputFields[i].value < 5){
+       console.log('inputFields[i].value:', inputFields[i].value);
        submitButton.disabled = true;
      } else {
       submitButton.disabled = false;
