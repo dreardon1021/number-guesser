25552a6ae6b70c802e8654352652e7184e4e8b2a
Dan Reardon <dreardon1021@gmail.com>
2019-12-05 14:38:40 -0700

    1st attmept enable/disable button

---
+++ main.js
@@ -0,0 +1,42 @@
+var nameGuessInput = document.querySelectorAll('.middle-left-input');
+var submitButton = document.querySelector('.submit-button');
+
+
+// funtion activateDeactivate() {
+// for(var i = 0; i < nameGuessInput.length; i++){
+// nameGuessInput.addEventListener('change', inputText)
+//   };
+//   if (nameGuessInput.length < 1){
+//     submitButton.setAttribute = ('disabled', 'disabled');
+//     console.log(disable)
+//   } else {
+//     submitButton.removeAttribute = ('disabled');
+//     console.log(enable)
+//   };
+// };
+
+
+
+
+
+// submitButton.addEventListener('click', poof)
+
+// function poof(){
+//   for(var i = 0; i < nameGuessInput.length; i++){
+//   nameGuessInput[i].style.display = 'none'
+//   };
+// };
+
+
+// submitButton.addEventListener('change', enableSubmit)
+
+
+// function enableSubmit(){
+// if (nameGuessInput.length < 1){
+//   submitButton.setAttribute = ('disabled', 'disabled');
+//   console.log(disable)
+// } else {
+//   submitButton.removeAttribute = ('disabled');
+//   console.log(enable)
+//     };
+// };
