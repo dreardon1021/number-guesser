72f8186fa3dc0d31acd7dc049275642d59744fc3
Cristina Pena <cristinalpena@gmail.com>
2019-12-05 20:21:42 -0700

    Add form, button disabled attribute, enable-button class in HTML and CSS. Enable submit button and tweak JS functions to work together

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.querySelector('.clear-form-button').addEventListener('click', clearFormInput);
 document.querySelector('.submit-form-button').addEventListener('click', clearGuessInput);
+document.querySelector('form').addEventListener('keyup', enableSubmit);
+
 
 function clearFormInput() {
   var inputFields = document.querySelectorAll('.middle-left-input');
@@ -10,7 +12,22 @@ function clearFormInput() {
 
 function clearGuessInput() {
   var guessInputFields = document.querySelectorAll('.guess-input-field');
+  var submitButton = document.querySelector('.submit-form-button');
   for (var i=0; i < guessInputFields.length; i++){
     guessInputFields[i].value = null;
-  }
+  };
+  submitButton.setAttribute('disabled', "");
+  submitButton.classList.remove('enable-button');
 }
+
+function enableSubmit() {
+  var submitButton = document.querySelector('.submit-form-button');
+  var gameForm = document.querySelector('form');
+  if (gameForm.checkValidity() === true){
+    submitButton.removeAttribute('disabled');
+    submitButton.classList.add('enable-button');
+  } else if (gameForm.checkValidity() === false){
+    submitButton.setAttribute('disabled', "");
+    submitButton.classList.remove('enable-button');
+    };
+  };
